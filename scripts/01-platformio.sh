fc2700e451424e62a3194ca563f158a46aea94d7
Peter Buchegger <peter@pbuchegger.at>
2022-11-05 21:44:01 +0100

    change in platformio install

--- scripts/01-platformio.sh
+++ scripts/01-platformio.sh
@@ -6,16 +6,10 @@ export LC_ALL=C
 source /common.sh
 install_cleanup_trap
 
-apt-get install -y python3-venv
+apt-get install -y python3-pip
 
-python3 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+pip install -U platformio
 
-mkdir -p /usr/local/bin
-ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
-ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
-ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
-
-# Recommended
 curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/master/scripts/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
 
 sudo usermod -a -G dialout pi
