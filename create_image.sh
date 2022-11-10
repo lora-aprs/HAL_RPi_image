a821e40b5bdca31cfcf71bf5d69a4bd2c9cc9997
Peter Buchegger <peter@pbuchegger.at>
2022-11-10 23:20:47 +0100

    because of issues with 64bit, we will use 32bit from now on

--- create_image.sh
+++ create_image.sh
@@ -3,7 +3,7 @@
 if [ ! -f input.img ]
 then
   echo "Downloading raspios image ..."
-  wget https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2022-09-26/2022-09-22-raspios-bullseye-arm64-lite.img.xz
+  wget https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2022-09-26/2022-09-22-raspios-bullseye-armhf-lite.img.xz
   unxz *.img.xz
   mv *.img input.img
 fi
