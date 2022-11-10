7c76b301f070601c969dbfa5aa23dc649270b66d
Peter Buchegger <peter@pbuchegger.at>
2022-11-10 23:21:11 +0100

    perform git clone as user pi

--- scripts/99-project.sh
+++ scripts/99-project.sh
@@ -6,7 +6,8 @@ export LC_ALL=C
 source /common.sh
 install_cleanup_trap
 
-git clone https://github.com/lora-aprs/LoRa_APRS_iGate.git
+cd /home/pi
+sudo -u pi git clone https://github.com/lora-aprs/LoRa_APRS_iGate.git
 cd LoRa_APRS_iGate
-pio pkg install -p "espressif32 @ 3.1.1"
-pio pkg install --tool "tool-mkspiffs"
+sudo -u pi pio pkg install -p "espressif32 @ 3.1.1"
+sudo -u pi pio pkg install --tool "tool-mkspiffs"
