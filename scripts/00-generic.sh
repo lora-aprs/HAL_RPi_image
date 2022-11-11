c97d6974d0168c9f826b383932ad573de6f68d9b
Peter Buchegger <peter@pbuchegger.at>
2022-11-11 20:51:13 +0100

    install ohmyzsh

--- scripts/00-generic.sh
+++ scripts/00-generic.sh
@@ -6,6 +6,10 @@ export LC_ALL=C
 source /common.sh
 install_cleanup_trap
 
-apt-get install -y zsh git vim pigpio python3-pigpio
+apt-get install -y zsh git vim
 chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo -u pi sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+apt-get install -y pigpio python3-pigpio
 systemctl enable pigpiod
