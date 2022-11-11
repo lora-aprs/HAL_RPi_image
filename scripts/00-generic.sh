f68aff18f143a4381b28fefcfae9144237cec709
Peter Buchegger <peter@pbuchegger.at>
2022-11-11 19:47:35 +0100

    use zsh as default shell

--- scripts/00-generic.sh
+++ scripts/00-generic.sh
@@ -6,5 +6,6 @@ export LC_ALL=C
 source /common.sh
 install_cleanup_trap
 
-apt-get install -y git vim pigpio python3-pigpio
+apt-get install -y zsh git vim pigpio python3-pigpio
+chsh -s /bin/zsh
 systemctl start pigpiod
